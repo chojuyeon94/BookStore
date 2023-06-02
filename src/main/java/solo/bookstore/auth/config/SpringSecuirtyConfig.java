e56130b0109d116637b614976c30ae42b50c4c3e
chojuyeon94 <chojuyeoun@gmail.com>
2023-06-03 02:16:46 +0900

    security 및 jwt인증 구현 진행 및 yml파일 설정 추가

--- src/main/java/solo/bookstore/auth/config/SpringSecuirtyConfig.java
+++ src/main/java/solo/bookstore/auth/config/SpringSecuirtyConfig.java
@@ -1,9 +1,56 @@
 package solo.bookstore.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import solo.bookstore.auth.jwt.JwtProvider;
+import solo.bookstore.auth.repository.RefreshTokenRepository;
+import solo.bookstore.auth.utils.CustomAuthUtils;
+import solo.bookstore.domain.member.repository.MemberRepository;
+import solo.bookstore.domain.member.service.MemberService;
 
 @Configuration
 @CrossOrigin
 public class SpringSecuirtyConfig {
+
+    private final JwtProvider jwtProvider;
+
+    private final CustomAuthUtils customAuthUtils;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final MemberService memberService;
+
+
+    public SpringSecuirtyConfig(JwtProvider jwtProvider, CustomAuthUtils customAuthUtils, RefreshTokenRepository refreshTokenRepository, MemberRepository memberRepository, MemberService memberService){
+        this.jwtProvider = jwtProvider;
+        this.customAuthUtils = customAuthUtils;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.memberRepository = memberRepository;
+        this.memberService = memberService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint()
+
+        return http.build();
+    }
+
+
 }
